7b573c39cf7e13bed45c4b91edca0de03ecf7ec1
yslee <yslee.dev@gmail.com>
2022-03-10 23:32:50 +0900

    pkg: update setup

---
+++ __init__.py
@@ -0,0 +1,1 @@
+__all__ = ["parquet_dataset"]
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = "0.0.1.dev3"
+
+project_urls = {
+    "Source": "https://github.com/rapidrabbit76/parquet-dataset",
+}
+install_requires = [
+    "pandas>=1.4.1",
+    "pyarrow>=7.0.0",
+    "torch>=1.6",
+]
+
+setup(
+    name="parquet-dataset",
+    version=version,
+    description="Dataset for parquet group",
+    author="yslee",
+    author_email="yslee.dev@gmail.com",
+    maintainer="yslee",
+    maintainer_email="yslee.dev@gamil.com",
+    license="MIT",
+    keywords="parquet pytorch dataset",
+    project_urls=project_urls,
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    packages=find_packages(),
+)
